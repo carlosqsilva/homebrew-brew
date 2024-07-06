dde972110a5b535b262f5cc7837d04d5900fd954
Carlos Silva <carlosqsilva@outlook.com>
2024-07-06 15:22:04 -0300

    chore: update vfetch

--- Formula/vfetch@0.2.2.rb
+++ Formula/vfetch@0.2.2.rb
@@ -1,8 +1,8 @@
 class Vfetch < Formula
   desc "System fetch tool for macOS written in vlang"
   homepage "https://github.com/carlosqsilva/vfetch"
-  url "https://github.com/carlosqsilva/vfetch/archive/refs/tags/0.2.3.tar.gz"
-  sha256 "6283cc8b8a5177d04a253c6728fc37ff0ce2f9ea14b8d936bc95f2a75e3252b6"
+  url "https://github.com/carlosqsilva/vfetch/archive/refs/tags/0.2.2.tar.gz"
+  sha256 "d53297a1ac58523979ad5331204fb9eeaca6496a4174dae094cf1d47bd2c8b61"
   license "MIT"
 
   depends_on "vlang" => ["0.4.6", :build]
