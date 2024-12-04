14c7adf97d741992241bef32316c639a71755383
Carlos Silva <carlosqsilva@outlook.com>
2024-12-03 22:01:58 -0300

    fix vqrcode version

--- Formula/vqrcode.rb
+++ Formula/vqrcode.rb
@@ -1,8 +1,8 @@
 class Vqrcode < Formula
   desc "CLI for creating QR codes written in vlang"
   homepage "https://github.com/carlosqsilva/vqrcode"
-  url "https://github.com/carlosqsilva/vqrcode/archive/refs/tags/0.3.1.tar.gz"
-  sha256 "54de34639dbe0606b5545e0be378be40724e5229780cf4caea63d44e2408553d"
+  url "https://github.com/carlosqsilva/vqrcode/archive/refs/tags/0.3.2.tar.gz"
+  sha256 "cb430b5a02f094497c1c06658102fac4216a8f3837a6c0dca7b4c712ca8e5824"
   license "MIT"
 
   depends_on "vlang" => ["0.4.8", :build]
