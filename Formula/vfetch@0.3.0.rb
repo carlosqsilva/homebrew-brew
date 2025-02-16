9b2ecd9e8db3e40877500857b242284c3d0dc7d6
Carlos Silva <carlosqsilva@outlook.com>
2025-02-16 13:06:40 -0300

    fix vfetch 0.3.0 version

--- Formula/vfetch@0.3.0.rb
+++ Formula/vfetch@0.3.0.rb
@@ -1,11 +1,11 @@
 class Vfetch < Formula
   desc "System fetch tool for macOS written in vlang"
   homepage "https://github.com/carlosqsilva/vfetch"
-  url "https://github.com/carlosqsilva/vfetch/archive/refs/tags/0.2.3.tar.gz"
-  sha256 "6283cc8b8a5177d04a253c6728fc37ff0ce2f9ea14b8d936bc95f2a75e3252b6"
+  url "https://github.com/carlosqsilva/vfetch/archive/refs/tags/0.3.0.tar.gz"
+  sha256 "ea36a9ca3df4196eea5306165282bdfbad4748197355dc6de4053562b068c697"
   license "MIT"
 
-  depends_on "vlang" => ["0.4.6", :build]
+  depends_on "vlang" => ["0.4.9", :build]
 
   def install
     system "v", "-prod", "-w", "-o", "vfetch", "."
